b9763fc9359b441e8588cdb9407bff0c4cf8d022
hxw <1064599729@qq.com>
2023-05-11 17:34:20 +0800

    修改

--- base-code/puzzlegame/src/com/ithuangxianwei/ui/GameJFrame.java
+++ base-code/puzzlegame/src/com/ithuangxianwei/ui/GameJFrame.java
@@ -2,11 +2,16 @@ package com.ithuangxianwei.ui;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
-public class GameJFrame extends JFrame {
+public class GameJFrame extends JFrame implements KeyListener {
     int[][] dataImage = new int[4][4];
-
+    // x y 当前0位置图片的位置
+    int x;
+    int y;
+    String path = "puzzlegame\\image\\animal\\animal3\\";
     public GameJFrame() {
         // 初始化界面
         initJFrame();
@@ -31,29 +36,32 @@ public class GameJFrame extends JFrame {
         }
 
         for (int i = 0; i < tempArr.length; i++) {
-            System.out.print(tempArr[i] + " ");
+            if (tempArr[i] == 0) {
+                x = i / 4;
+                y = i % 4;
+            }
             dataImage[i / 4][i % 4] = tempArr[i];
         }
     }
 
     // 初始化图片
     private void initImages() {
+        this.getContentPane().removeAll();
         // 添加背景图片
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                int num = dataImage[j][i];
-                System.out.println(num);
-                JLabel jLabel = new JLabel(new ImageIcon("puzzlegame\\image\\animal\\animal3\\" + num + ".jpg"));
+                int num = dataImage[i][j];
+                JLabel jLabel = new JLabel(new ImageIcon(path + num + ".jpg"));
 
                 jLabel.setBounds(105 * j + 83, 105 * i + 134, 105, 105);
                 jLabel.setBorder(new BevelBorder(1));
                 this.getContentPane().add(jLabel);
-
             }
         }
         JLabel bg = new JLabel(new ImageIcon("puzzlegame\\image\\background.png"));
         bg.setBounds(40, 40, 508, 560);
         this.getContentPane().add(bg);
+        this.getContentPane().repaint();
     }
 
 
@@ -84,5 +92,55 @@ public class GameJFrame extends JFrame {
         this.setDefaultCloseOperation(3);
         // 取消默认布局
         this.setLayout(null);
+        this.addKeyListener(this);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+        System.out.println(code);
+        if (code == 37) {
+            if (y == 3) {
+               return;
+            }
+            dataImage[x][y] = dataImage[x][y + 1];
+            dataImage[x][y + 1] = 0;
+            y++;
+            initImages();
+        } else if (code == 38) {
+            if (x == 3) {
+                return;
+            }
+            dataImage[x][y] = dataImage[x + 1][y];
+            dataImage[x + 1][y] = 0;
+            x++;
+            initImages();
+        } else if (code == 39) {
+            if (y == 0) {
+                return;
+            }
+            dataImage[x][y] = dataImage[x][y - 1];
+            dataImage[x][y - 1] = 0;
+            y--;
+            initImages();
+        } else if (code == 40) {
+            if (x == 0) {
+                return;
+            }
+            dataImage[x][y] = dataImage[x - 1][y];
+            dataImage[x - 1][y] = 0;
+            x--;
+            initImages();
+        }
     }
 }
