8ba8b0f65e942d4257c27893181b8a1d75aff3a4
hxw <1064599729@qq.com>
2023-05-25 13:45:36 +0800

    学习

--- base-code/day02-code/src/com/itheima/demo1/HelloWorld1.java
+++ base-code/day02-code/src/com/itheima/demo1/HelloWorld1.java
@@ -1,9 +1,15 @@
 package com.itheima.demo1;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class HelloWorld1 {
     public static void main(String[] args) {
-        System.out.println("args = " + Arrays.toString(args));
+        Map<String, String> map = new HashMap<>();
+        map.put("标枪选手", "马超");
+        map.put("人物挂件", "明世隐");
+        map.put("御龙骑士", "尹志平");
+        Set<Map.Entry<String, String>> entries = map.entrySet();
     }
 }
